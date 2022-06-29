1d76060b6dfdc25663d6dce28bd1ebe3908ac7f3
gmercon <gmercon@knights.ucf.edu>
2022-06-29 10:59:43 -0400

    Create Assignment02GM.py

---
+++ Assignment02/Assignment02GM.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 29 10:25:41 2022
+
+@author: Guillermo
+"""
+
+# 1 Assign a variable name to each of the following and use Python to identify the 
+# data type of each variable:
+# 2
+# 2.0
+# 10j
+# 2 Cool for School
+# True
+
+Two = 2 
+
+TwoPoint0 = 2.0
+
+TenJ = 10j
+
+Dog = "2 cool for School"
+
+Correct = True
+
